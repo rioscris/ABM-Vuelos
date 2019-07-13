0b8dbecb75ca2ff634f80b469343a49345450433
rioscris <rios7563@gmail.com>
2019-07-12 23:43:40 -0300

    Definido el modelo Vuelo

---
+++ ABMVuelos/Modelos/Vuelo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ABMVuelos.Modelos
+{
+    public class Vuelo
+    {
+        public int ID { get; set; }
+        
+        [DataType(DataType.Time)]
+        public DateTime llegada { get; set; }
+
+        
+        public string Linea { get; set; }
+
+        
+        public string Codigo { get; set; }
+        
+        public Boolean Demorado { get; set; }
+    }
+}
